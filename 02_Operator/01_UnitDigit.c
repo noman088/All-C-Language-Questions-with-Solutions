85aaa4cc6d98df4968fc126b089f48505d8126e7
Noman <mdnoman70499@gmail.com>
2023-11-12 23:31:43 +0530

    Make file of Each Question

---
+++ 02_Operator/01_UnitDigit.c
@@ -0,0 +1,12 @@
+// //1. Write a program  to print unit digit of a given number
+
+// #include <stdio.h>
+// #include <conio.h>
+// int main()
+// {
+//     int Num;
+//     printf("Enter a Number ");
+//     scanf("%d", &Num);
+//     Num = Num % 10;
+//     printf("The Unit Number of A given Number is %d", Num);
+// }
---
+++ 02_Operator/02_NumWithoutLastDigit.c
@@ -0,0 +1,14 @@
+
+// //2. Write a program to print a given number without its last digit.
+
+// #include <stdio.h>
+// #include <conio.h>
+// int main()
+// {
+//     int num;
+//     printf("Ente a Number ");
+//     scanf("%d", &num);
+//     num = num / 10;
+//     printf("Number is without last digit is %d", num);
+//     return 0;
+// }
