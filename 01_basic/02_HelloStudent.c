87523a9a9c612623f05238ec9f7a7d880cfbe4ce
Noman <mdnoman70499@gmail.com>
2023-11-26 23:52:39 +0530

    Q2 Done

---
+++ 01_basic/02_HelloStudent.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+
+    // printf("Hello\nStudents");
+    // printf("Hello");
+    // printf("\n");
+    // printf("Students");
+    printf("Hello\n");
+    printf("Students");
+    return 0
+}
