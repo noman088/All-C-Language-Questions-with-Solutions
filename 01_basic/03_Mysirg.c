ddfbaccc2a3bb6e0e913a87ba2535864f5339871
Noman <mdnoman70499@gmail.com>
2023-12-15 23:18:00 +0530

    today commit

--- 01_basic/03_Mysirg.c
+++ 01_basic/03_Mysirg.c
@@ -1,9 +1,7 @@
 // 3. Write a program to print “MySirG” on the screen.(Remember to print in double quotes)
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-// How are you my friends
-// Just doing like I have nothing to do 
     printf("\"MySirG\"");
     return 0;
 }
---
+++ 01_basic/05_StrLenPrintf.c
@@ -0,0 +1,14 @@
+// 5. WAP to calculate the length of String using printf function.
+
+
+// example = "noman"
+#include <stdio.h>
+int main()
+{
+    int x;
+    x = printf("noman");
+    printf("\n");
+    printf("length of String is %d", x);
+
+    return 0;
+}
