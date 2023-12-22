bafafd7b2482e0f83ab606d808e26d66a77d55ee
Noman <mdnoman70499@gmail.com>
2023-12-23 00:23:02 +0530

    first commit

---
+++ 01_basic/06_HelloUser.c
@@ -0,0 +1,9 @@
+// 6. WAP to print the name of the user in double quotes.
+// Expected output format – “Hello, Amit Kumar”
+#include<stdio.h>
+int main()
+{
+    // \" -- print double quotte 
+    printf("\"Hello , Amit Kumar\"");
+    return 0;
+}
