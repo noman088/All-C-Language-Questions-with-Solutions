9dd9d1be08f563adb42241549887c83c669d5d72
Noman <mdnoman70499@gmail.com>
2023-12-23 00:43:37 +0530

    done a6

--- 01_basic/06_HelloUser.c
+++ 01_basic/06_HelloUser.c
@@ -1,9 +1,13 @@
 // 6. WAP to print the name of the user in double quotes.
 // Expected output format – “Hello, Amit Kumar”
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    // \" -- print double quotte 
-    printf("\"Hello , Amit Kumar\"");
+    char name[50];
+    printf("Please Enter your Name Here : ");
+    gets(name);
+    // \"-- Print Qoute in qoute 
+    printf("\"Hello , %s\"", name);
+
     return 0;
 }
