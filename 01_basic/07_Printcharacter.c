4711a19a95aced2967334d7e6060efe689420d76
Noman <mdnoman70499@gmail.com>
2023-12-27 23:19:00 +0530

    Question 7 Solution :

---
+++ 01_basic/07_Printcharacter.c
@@ -0,0 +1,14 @@
+// 7. WAP to print “%d” on the screen.
+
+#include<stdio.h>
+int main()
+{
+    // char a = '%' ;
+    // char b = 'd';
+
+    // printf("%c%c",a,b);
+    printf("%%d");
+
+    return 0;
+
+}
