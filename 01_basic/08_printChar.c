74552710964e44f0a24ad1d59fee066bc5630d16
Noman <mdnoman70499@gmail.com>
2023-12-28 22:24:35 +0530

    Q8 solution added:

---
+++ 01_basic/08_printChar.c
@@ -0,0 +1,12 @@
+// 8. WAP to print “\n” on the screen.
+
+#include <stdio.h>
+int main()
+{
+    // Escape secquence - \n -- New line
+    // \\ - \
+    printf("\n");
+    printf("\\n");
+
+    return 0;
+}
