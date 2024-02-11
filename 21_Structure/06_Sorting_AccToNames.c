185346cbbf81468a762c65350a01c937eefc5cd4
Noman Ahmad <mdnoman70499@gmail.com>
2024-02-11 23:37:42 +0530

    made some changes

--- 21_Structure/06_Sorting_AccToNames.c
+++ 21_Structure/06_Sorting_AccToNames.c
@@ -1,2 +1,26 @@
 // 6. Write a function to sort employees according to their names [refer structure from
 // question 1]
+#include <stdio.h>
+struct Emp
+{
+        int id;
+        char name[20];
+        float salary;
+};
+struct Emp SetData(struct Emp *employee)
+{
+        printf("Enter Id : \n");
+        scanf("%d", &employee->id);
+        fflush(stdin);
+        printf("Enter Employee Name : ");
+        fgets(employee->name, sizeof(employee->name), stdin);
+        
+        
+}
+
+int main()
+{
+        struct Emp mister, noman, aarif;
+
+        return 0;
+}
